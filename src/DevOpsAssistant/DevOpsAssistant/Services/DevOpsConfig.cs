a01d1d5dcac56f1c7c6cb37b04db9c0c50888986
Tom Carr <52778130+tommcarrr@users.noreply.github.com>
2025-06-04 22:46:26 +0100

    Add MudBlazor settings dialog with local storage

---
+++ src/DevOpsAssistant/DevOpsAssistant/Services/DevOpsConfig.cs
@@ -0,0 +1,9 @@
+namespace DevOpsAssistant.Services
+{
+    public class DevOpsConfig
+    {
+        public string Organization { get; set; } = string.Empty;
+        public string Project { get; set; } = string.Empty;
+        public string PatToken { get; set; } = string.Empty;
+    }
+}
