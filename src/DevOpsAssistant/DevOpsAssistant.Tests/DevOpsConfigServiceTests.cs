fd34cd491a0865284216f59a32236eda2cdb01aa
Tom Carr <52778130+tommcarrr@users.noreply.github.com>
2025-06-05 07:23:42 +0100

    Add unit tests for DevOpsConfigService

---
+++ src/DevOpsAssistant/DevOpsAssistant.Tests/DevOpsConfigServiceTests.cs
@@ -0,0 +1,57 @@
+using System.Threading.Tasks;
+using DevOpsAssistant.Services;
+using Xunit;
+
+namespace DevOpsAssistant.Tests;
+
+public class DevOpsConfigServiceTests
+{
+    [Fact]
+    public async Task SaveAsync_Persists_Config_And_Updates_Property()
+    {
+        var storage = new FakeLocalStorageService();
+        var service = new DevOpsConfigService(storage);
+        var config = new DevOpsConfig
+        {
+            Organization = "Org",
+            Project = "Proj",
+            PatToken = "Token"
+        };
+
+        await service.SaveAsync(config);
+
+        Assert.Equal("Org", service.Config.Organization);
+        var stored = await storage.GetItemAsync<DevOpsConfig>("devops-config");
+        Assert.Equal("Org", stored.Organization);
+        Assert.Equal("Proj", stored.Project);
+        Assert.Equal("Token", stored.PatToken);
+    }
+
+    [Fact]
+    public async Task LoadAsync_Loads_Config_When_Present()
+    {
+        var storage = new FakeLocalStorageService();
+        var stored = new DevOpsConfig { Organization = "Org", Project = "Proj", PatToken = "Token" };
+        await storage.SetItemAsync("devops-config", stored);
+        var service = new DevOpsConfigService(storage);
+
+        await service.LoadAsync();
+
+        Assert.Equal("Org", service.Config.Organization);
+        Assert.Equal("Proj", service.Config.Project);
+        Assert.Equal("Token", service.Config.PatToken);
+    }
+
+    [Fact]
+    public async Task LoadAsync_Keeps_Default_When_No_Config()
+    {
+        var storage = new FakeLocalStorageService();
+        var service = new DevOpsConfigService(storage);
+
+        await service.LoadAsync();
+
+        Assert.Equal(string.Empty, service.Config.Organization);
+        Assert.Equal(string.Empty, service.Config.Project);
+        Assert.Equal(string.Empty, service.Config.PatToken);
+    }
+}
