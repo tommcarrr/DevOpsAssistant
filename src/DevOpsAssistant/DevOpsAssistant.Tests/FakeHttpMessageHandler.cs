3f49173f449742b0e1ea18dcce62cf147bb5b020
Tom Carr <52778130+tommcarrr@users.noreply.github.com>
2025-06-05 16:37:50 +0100

    Add additional unit and front-end tests

---
+++ src/DevOpsAssistant/DevOpsAssistant.Tests/FakeHttpMessageHandler.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace DevOpsAssistant.Tests;
+
+public class FakeHttpMessageHandler : HttpMessageHandler
+{
+    private readonly Func<HttpRequestMessage, HttpResponseMessage> _handler;
+
+    public FakeHttpMessageHandler(Func<HttpRequestMessage, HttpResponseMessage> handler)
+    {
+        _handler = handler;
+    }
+
+    protected override Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
+    {
+        return Task.FromResult(_handler(request));
+    }
+}
