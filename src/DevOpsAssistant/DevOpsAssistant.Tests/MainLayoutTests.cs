52fb57941b4d8c85b6e277c1aedb8f338dca63a6
Tom Carr <52778130+tommcarrr@users.noreply.github.com>
2025-06-05 21:13:12 +0100

    Add splash screen when settings missing

--- src/DevOpsAssistant/DevOpsAssistant.Tests/MainLayoutTests.cs
+++ src/DevOpsAssistant/DevOpsAssistant.Tests/MainLayoutTests.cs
@@ -37,4 +37,33 @@ public class MainLayoutTests : TestContext
 
         Assert.Contains("--mud-native-html-color-scheme: dark", cut.Markup);
     }
+
+    [Fact]
+    public void Layout_Shows_Splash_When_Config_Incomplete()
+    {
+        Services.AddMudServices();
+        JSInterop.Mode = JSRuntimeMode.Loose;
+        Services.AddSingleton(new DevOpsConfigService(new FakeLocalStorageService()));
+
+        var cut = RenderComponent<MainLayout>();
+
+        Assert.Contains("Configuration Required", cut.Markup);
+        Assert.Contains("mud-nav-link-disabled", cut.Markup);
+    }
+
+    [Fact]
+    public async Task Layout_Hides_Splash_When_Config_Complete()
+    {
+        Services.AddMudServices();
+        JSInterop.Mode = JSRuntimeMode.Loose;
+        var storage = new FakeLocalStorageService();
+        var configService = new DevOpsConfigService(storage);
+        await configService.SaveAsync(new DevOpsConfig { Organization = "Org", Project = "Proj", PatToken = "token" });
+        Services.AddSingleton(configService);
+
+        var cut = RenderComponent<MainLayout>();
+
+        Assert.DoesNotContain("Configuration Required", cut.Markup);
+        Assert.DoesNotContain("mud-nav-link-disabled", cut.Markup);
+    }
 }
