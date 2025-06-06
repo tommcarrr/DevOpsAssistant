447591413e95013f9450e5a6859f8287c3d1b66f
Tom Carr <52778130+tommcarrr@users.noreply.github.com>
2025-06-06 12:48:08 +0100

    Update release notes selection

--- src/DevOpsAssistant/DevOpsAssistant/Services/Models/WorkItemInfo.cs
+++ src/DevOpsAssistant/DevOpsAssistant/Services/Models/WorkItemInfo.cs
@@ -1,10 +1,16 @@
 namespace DevOpsAssistant.Services;
 
-public class WorkItemInfo
+public class WorkItemInfo : IEquatable<WorkItemInfo>
 {
     public int Id { get; set; }
     public string Title { get; set; } = string.Empty;
     public string State { get; set; } = string.Empty;
     public string WorkItemType { get; set; } = string.Empty;
     public string Url { get; set; } = string.Empty;
+
+    public bool Equals(WorkItemInfo? other) => other != null && other.Id == Id;
+
+    public override bool Equals(object? obj) => obj is WorkItemInfo wi && Equals(wi);
+
+    public override int GetHashCode() => Id.GetHashCode();
 }
