a8f988f722d8d0b95ba3f6c844237341cdefb040
Tom Carr <52778130+tommcarrr@users.noreply.github.com>
2025-06-17 20:57:26 +0100

    Load selected types from state

--- src/DevOpsAssistant/DevOpsAssistant.Tests/Pages/ValidationPageTests.cs
+++ src/DevOpsAssistant/DevOpsAssistant.Tests/Pages/ValidationPageTests.cs
@@ -5,6 +5,10 @@ using DevOpsAssistant.Tests.Utils;
 using System.Collections;
 using System.Collections.Generic;
 using System.Reflection;
+using System.Net;
+using System.Net.Http;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Localization;
 
 namespace DevOpsAssistant.Tests.Pages;
 
@@ -66,6 +70,56 @@ public class ValidationPageTests : ComponentTestBase
 
         cut.Find("button.rules-toggle").Click();
         cut.WaitForAssertion(() => Assert.True((bool)expandedField.GetValue(cut.Instance)!));
+    }
+
+    [Fact]
+    public async Task OnInitialized_Loads_Types_From_State()
+    {
+        var config = SetupServices(includeApi: true);
+        await config.SaveAsync(new DevOpsConfig
+        {
+            Organization = "Org",
+            Project = "Proj",
+            PatToken = "token"
+        });
+
+        var handler = new FakeHttpMessageHandler(req =>
+        {
+            if (req.RequestUri!.AbsoluteUri.Contains("classificationnodes"))
+                return new HttpResponseMessage(HttpStatusCode.OK) { Content = new StringContent("{\"path\":\"Area\"}") };
+            if (req.RequestUri!.AbsoluteUri.Contains("states"))
+                return new HttpResponseMessage(HttpStatusCode.OK) { Content = new StringContent("{\"value\":[{\"name\":\"New\"}]}") };
+            return new HttpResponseMessage(HttpStatusCode.OK) { Content = new StringContent("{}") };
+        });
+        var client = new HttpClient(handler);
+        Services.AddSingleton(new DeploymentConfigService(new HttpClient()));
+        Services.AddSingleton<DevOpsApiService>(sp => new DevOpsApiService(
+            client,
+            config,
+            sp.GetRequiredService<DeploymentConfigService>(),
+            sp.GetRequiredService<IStringLocalizer<DevOpsApiService>>()));
+
+        var stateService = Services.GetRequiredService<PageStateService>();
+        await stateService.SaveAsync("validation", new TestState
+        {
+            Path = "Area",
+            States = new[] { "New" },
+            Types = new[] { "Bug" }
+        });
+
+        var cut = RenderWithProvider<Validation>();
+
+        var prop = typeof(Validation).GetProperty("SelectedTypes", BindingFlags.NonPublic | BindingFlags.Instance)!;
+        var set = (HashSet<string>)prop.GetValue(cut.Instance)!;
+        Assert.Single(set);
+        Assert.Contains("Bug", set);
+    }
+
+    private class TestState
+    {
+        public string Path { get; set; } = string.Empty;
+        public string[]? States { get; set; }
+        public string[]? Types { get; set; }
     }
 
     private class TestPage : Validation
