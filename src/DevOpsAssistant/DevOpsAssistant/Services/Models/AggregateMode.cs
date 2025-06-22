8eeead22e9b1dbb4f4ec483a7676fb56ef4f8151
Tom Carr <52778130+tommcarrr@users.noreply.github.com>
2025-06-22 21:47:51 +0100

    Add iteration aggregation to metrics

--- src/DevOpsAssistant/DevOpsAssistant/Services/Models/AggregateMode.cs
+++ src/DevOpsAssistant/DevOpsAssistant/Services/Models/AggregateMode.cs
@@ -4,5 +4,6 @@ public enum AggregateMode
 {
     Week,
     Fortnight,
-    Month
+    Month,
+    Iteration
 }
---
+++ src/DevOpsAssistant/DevOpsAssistant/Services/Models/IterationInfo.cs
@@ -0,0 +1,8 @@
+namespace DevOpsAssistant.Services.Models;
+
+public class IterationInfo
+{
+    public string Name { get; set; } = string.Empty;
+    public DateTime StartDate { get; set; }
+    public DateTime EndDate { get; set; }
+}
