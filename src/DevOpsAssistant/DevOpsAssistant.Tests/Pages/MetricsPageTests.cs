8d25806b58af3e98bfae348dc70e4997b739c795
Tom Carr <52778130+tommcarrr@users.noreply.github.com>
2025-06-25 17:59:27 +0100

    Rename target points to additional points

--- src/DevOpsAssistant/DevOpsAssistant.Tests/Pages/MetricsPageTests.cs
+++ src/DevOpsAssistant/DevOpsAssistant.Tests/Pages/MetricsPageTests.cs
@@ -96,7 +96,7 @@ public class MetricsPageTests : ComponentTestBase
 
         var metrics = new TestMetrics();
         var type = typeof(Metrics);
-        type.GetField("_targetPoints", BindingFlags.NonPublic | BindingFlags.Instance)!.SetValue(metrics, (double?)10);
+        type.GetField("_additionalPoints", BindingFlags.NonPublic | BindingFlags.Instance)!.SetValue(metrics, (double?)10);
         type.GetField("_efficiency", BindingFlags.NonPublic | BindingFlags.Instance)!.SetValue(metrics, (double?)80);
         type.GetField("_errorRange", BindingFlags.NonPublic | BindingFlags.Instance)!.SetValue(metrics, (double?)10);
         var compute = type.GetMethod("ComputeBurnUp", BindingFlags.NonPublic | BindingFlags.Instance)!;
