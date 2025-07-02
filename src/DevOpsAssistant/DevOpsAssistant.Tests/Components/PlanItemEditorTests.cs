471119001b2f53beca4133e53370d1b149d37b23
Tom Carr <52778130+tommcarrr@users.noreply.github.com>
2025-07-02 22:25:18 +0100

    Add drag handle to plan items

--- src/DevOpsAssistant/DevOpsAssistant.Tests/Components/PlanItemEditorTests.cs
+++ src/DevOpsAssistant/DevOpsAssistant.Tests/Components/PlanItemEditorTests.cs
@@ -17,4 +17,16 @@ public class PlanItemEditorTests : ComponentTestBase
 
         cut.Find("div.inner").MarkupMatches("<div class=\"inner\">content</div>");
     }
+
+    [Fact]
+    public void Shows_DragHandle_When_Draggable()
+    {
+        SetupServices();
+        var cut = RenderComponent<PlanItemEditor>(p => p
+            .Add(c => c.Type, "Epic")
+            .Add(c => c.Draggable, true)
+        );
+
+        Assert.NotNull(cut.Find(".drag-handle"));
+    }
 }
