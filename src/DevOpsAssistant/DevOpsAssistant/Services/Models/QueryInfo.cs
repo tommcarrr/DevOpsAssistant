bd27a35daaf37c720429f0e6099f8b989c1ace23
Tom Carr <52778130+tommcarrr@users.noreply.github.com>
2025-07-03 11:54:29 +0100

    Add shared query support

---
+++ src/DevOpsAssistant/DevOpsAssistant/Services/Models/QueryInfo.cs
@@ -0,0 +1,8 @@
+namespace DevOpsAssistant.Services.Models;
+
+public class QueryInfo
+{
+    public string Id { get; set; } = string.Empty;
+    public string Name { get; set; } = string.Empty;
+    public string Path { get; set; } = string.Empty;
+}
