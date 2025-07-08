26058534db7ac6ec8dc896995716e811e1753644
Tom Carr <52778130+tommcarrr@users.noreply.github.com>
2025-07-08 16:24:18 +0100

    Add NFR coverage option

--- src/DevOpsAssistant/DevOpsAssistant/Services/PromptService.cs
+++ src/DevOpsAssistant/DevOpsAssistant/Services/PromptService.cs
@@ -163,7 +163,9 @@ public class PromptService
         if (config.Nfrs.Count == 0) return string.Empty;
 
         var sb = new StringBuilder();
-        sb.AppendLine(RequirementsPlanner_NonFunctionalIntroPrompt.Value);
+        sb.AppendLine(config.CoverNfrs
+            ? RequirementsPlanner_NonFunctionalCoverPrompt.Value
+            : RequirementsPlanner_NonFunctionalIgnorePrompt.Value);
         foreach (var nfr in config.Nfrs)
             sb.AppendLine($"- {nfr}");
         return sb.ToString();
